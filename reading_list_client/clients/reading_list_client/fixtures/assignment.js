88de8cab3503ea14d690b50940073cf4dfdcc3ce
Devin Chalmers <devin.chalmers@gmail.com>
2008-10-21 23:09:47 -0700

    More typos

--- reading_list_client/clients/reading_list_client/fixtures/assignment.js
+++ reading_list_client/clients/reading_list_client/fixtures/assignment.js
@@ -8,15 +8,15 @@ ReadingListClient.FIXTURES = ReadingListClient.FIXTURES.concat([
 
 	{ guid: 1, type: 'Assignment',
 		book: 1, student: 1 },
-	{ guid: 1, type: 'Assignment',
+	{ guid: 2, type: 'Assignment',
 		book: 2, student: 1 },
-	{ guid: 1, type: 'Assignment',
+	{ guid: 3, type: 'Assignment',
 		book: 3, student: 2 },
-	{ guid: 1, type: 'Assignment',
+	{ guid: 4, type: 'Assignment',
 		book: 1, student: 2 },
-	{ guid: 1, type: 'Assignment',
+	{ guid: 5, type: 'Assignment',
 		book: 3, student: 3 },
-	{ guid: 1, type: 'Assignment',
+	{ guid: 6, type: 'Assignment',
 		book: 4, student: 3 }
 
 ]);
